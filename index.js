b8c1fa92fe202b40a8abbd54854084b454e1802c
Peter Trajkovski <peter.trajkovski.1@gmail.com>
2019-02-25 22:22:32 +1100

    complete dice game

---
+++ index.js
@@ -0,0 +1,26 @@
+//Dice Number 1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png"; //images/dice1.png - images/dice6.png
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+
+
+//Dice Number 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//Change Title depending which dice wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
